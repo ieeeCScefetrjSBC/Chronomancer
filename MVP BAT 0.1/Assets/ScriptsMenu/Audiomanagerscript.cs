a7b4d82fdff6ad275e9bdfeeab78d84bf238023b
igor120o <igor120o@hotmail>
2018-11-15 19:14:45 -0200

    script de audio colocado (ainda n finalizei)

---
+++ MVP BAT 0.1/Assets/ScriptsMenu/Audiomanagerscript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiomanagerscript : MonoBehaviour {
+    public static AudioClip walksound, iceblocksound, chainsound, oilsound, meteorsound, rainsound;
+    static AudioSource audioSrc;
+    // Use this for initialization
+	void Start () {
+        walksound = Resources.Load<AudioClip>("walk");
+        iceblocksound = Resources.Load<AudioClip>("ice");
+        chainsound = Resources.Load<AudioClip>("chain");
+        rainsound = Resources.Load<AudioClip>("rain");
+         oilsound = Resources.Load<AudioClip>("oil");
+        meteorsound = Resources.Load<AudioClip>("meteor");
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+        public static void PlaySound (string clip)
+        {
+            switch (clip)
+            {
+                case "walk":
+                    audioSrc.PlayOneShot(walksound);
+                    break;
+                case "ice":
+                    audioSrc.PlayOneShot(iceblocksound);
+                    break;
+                case "chain":
+                    audioSrc.PlayOneShot(chainsound);
+                    break;
+                case "rain":
+                    audioSrc.PlayOneShot(rainsound);
+                    break;
+                case "oil":
+                    audioSrc.PlayOneShot(oilsound);
+                    break;
+                case "meteor":
+                    audioSrc.PlayOneShot(meteorsound);
+                    break;
+            }
+        }
+		
+	}
+
