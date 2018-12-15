0dc76f77dcb526f5334ebae36cb06bee32f8101a
RafaDdS <rafunga@gmail.com>
2018-12-15 11:52:10 -0200

    Da para jogar de inicio a fim com um bug só

--- MVP BAT 0.1/Assets/Mapa/GameManager.cs
+++ MVP BAT 0.1/Assets/Mapa/GameManager.cs
@@ -50,6 +50,7 @@ public class GameManager : MonoBehaviour {
         {
             MapManager.MM.InitMap(sequence[s, level]);
             level++;
+            pause.SkillsDel = false;
         }
         else
         {
