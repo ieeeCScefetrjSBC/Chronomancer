475216003df86087a4bca1cee3e2a2e1eb10df57
RafaDdS <rafunga@gmail.com>
2018-12-15 02:33:38 -0200

    iei

---
+++ MVP BAT 0.1/Assets/ArenaBoss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArenaBoss : MonoBehaviour {
+
+
+    public Image Life;
+    public Transform hora, minuto;
+    float posHora, posMin, maxLife;
+    SkillBoss Boss;
+
+    void Start () {
+        Boss = FindObjectOfType<SkillBoss>();
+        maxLife = Boss.vida;
+	}
+	
+	void Update () {
+        hora.rotation = Quaternion.Euler(posHora + 1, -90, 90);
+        posHora += 1;
+        minuto.rotation = Quaternion.Euler(posMin + 12, -90, 90);
+        posMin += 12;
+
+        Life.fillAmount = Boss.vida/maxLife;
+    }
+}
