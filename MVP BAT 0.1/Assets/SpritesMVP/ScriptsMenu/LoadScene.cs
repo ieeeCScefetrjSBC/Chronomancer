e296f755ac7fa78e50a8290d9d3400ce6a8b81d7
RafaDdS <rafunga@gmail.com>
2018-12-18 17:41:52 -0200

    Cena rincial morfa no dialogo

--- MVP BAT 0.1/Assets/SpritesMVP/ScriptsMenu/LoadScene.cs
+++ MVP BAT 0.1/Assets/SpritesMVP/ScriptsMenu/LoadScene.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 public class LoadScene : MonoBehaviour
 {
 
+    Scene Men;
+
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadAdd(int sceneIndex)
+    {
+        SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+        Men = SceneManager.GetActiveScene();
+        Invoke("Unload", 5);
+    }
+
+    public void Unload()
+    {
+        SceneManager.UnloadSceneAsync(Men);
+        Invoke("Unload", 5);
     }
 }
